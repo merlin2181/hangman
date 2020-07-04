54d047e52fcb5d21b83b8a64998408817842fd9d
merlin2181 <merlin2181@gmail.com>
2020-07-03 20:30:25 -0700

    Final stage of Hangman project on JetBrains Academy.

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -4,44 +4,56 @@ import string
 
 
 def check_input(text, listofguess):
-    if len(text) < 1 or len(text) > 1:
+    if len(text) != 1:
         print("You should input a single letter")
-        return 1
+        return True
     if text not in string.ascii_lowercase:
         print("It is not an ASCII lowercase letter")
-        return 1
+        return True
     if text in listofguess:
         print("You already typed this letter")
-        return 1
-    return 0
+        return True
+    return False
 
 
 word = ['python', 'java', 'kotlin', 'javascript']
-game_word = random.choice(word)
-set_word = set(game_word)
-output = list('-' * len(game_word))
-lives = 8
-guesses = []
 print("H A N G M A N")
-while lives > 0:
-    print("""
+while True:
+    game_word = random.choice(word)
+    set_word = set(game_word)
+    output = list('-' * len(game_word))
+    lives = 8
+    guesses = set()
+    while True:
+        keep_going = input('Type "play" to play the game, "exit" to quit: ')
+        if keep_going != "play" and keep_going != "exit":
+            continue
+        else:
+            break
+    if keep_going == "play":
+        while lives > 0:
+            print("""
 {}""".format(''.join(output)))
-    guess = input("Input a letter: ")
-    check = check_input(guess, guesses)
-    guesses.append(guess)
-    if check == 1:
-        continue
-    if guess in set_word:
-        for i in range(len(game_word)):
-            if game_word[i] == guess:
-                output[i] = guess
+            guess = input("Input a letter: ")
+            if check_input(guess, guesses):
+                continue
+            if guess in set_word:
+                for i in range(len(game_word)):
+                    if game_word[i] == guess:
+                        output[i] = guess
+                        guesses.add(guess)
+            else:
+                print("No such letter in the word")
+                guesses.add(guess)
+                lives -= 1
+            name = ("".join(output))
+            if name == game_word:
+                print("""You guessed the word {}!
+You survived!
+""".format(game_word))
+                break
+            if lives == 0:
+                print("""You are hanged!
+""")
     else:
-        print("No such letter in the word")
-        lives -= 1
-    name = ("".join(output))
-    if name == game_word:
-        print("""You guessed the word {}!
-You survived!""".format(game_word))
         break
-    if lives == 0:
-        print("You are hanged!")

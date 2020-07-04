c287aed0eb46ff7235a136a6efc1d5204975acad
merlin2181 <merlin2181@gmail.com>
2020-07-03 18:53:13 -0700

    stage 6 of Hangman project on JetBrains Academy.

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -5,20 +5,26 @@ word = ['python', 'java', 'kotlin', 'javascript']
 game_word = random.choice(word)
 set_word = set(game_word)
 output = list('-' * len(game_word))
-attempts = 8
+lives = 8
 print("H A N G M A N")
-while attempts > 0:
+while lives > 0:
     print("""
 {}""".format(''.join(output)))
     guess = input("Input a letter: ")
     if guess in set_word:
-        for i in range(len(game_word)):
-            if game_word[i] == guess:
-                output[i] = guess
-        attempts -= 1
+        if guess in output:
+            print("No improvements")
+            lives -= 1
+        else:
+            for i in range(len(game_word)):
+                if game_word[i] == guess:
+                    output[i] = guess
     else:
         print("No such letter in the word")
-        attempts -= 1
-print("""
-Thanks for playing!
-We'll see how well you did in the next stage""")
+        lives -= 1
+    if output == game_word:
+        print("""You guessed the word!
+You survived!""")
+        break
+    if lives == 0:
+        print("You are hanged!")
